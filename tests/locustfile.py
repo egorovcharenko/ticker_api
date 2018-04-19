08b0ccf3fb004e9f47cc47519353b3b40c60e5db
Egor <egor.ovcharenko@gmail.com>
2018-04-19 22:53:27 +0300

    Добавил кеширование

---
+++ tests/locustfile.py
@@ -0,0 +1,21 @@
+from locust import HttpLocust, TaskSet
+
+
+def get_price(l):
+    l.client.get("/ticker?pair=btc_usd")
+
+
+class UserBehavior(TaskSet):
+    tasks = {get_price: 1}
+
+    def on_start(self):
+        pass
+
+    def on_stop(self):
+        pass
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
--- ticker-api/code/app.py
+++ ticker-api/code/app.py
@@ -26,6 +26,10 @@ class DatabaseLayer:
         """Инициализировать поллер адресом биржи и интервалом"""
         self.db_connection = db_connection
 
+        self.cache = {}
+        self.all_pairs_cached = {}
+        self.all_pairs_cached_time = None
+
         # ожидаем подключения к БД - без этого продолжать нет смысла
         self.client = pymongo.MongoClient(db_connection)
         while True:
@@ -43,26 +47,48 @@ class DatabaseLayer:
 def get_ticker(pair: str = None) -> Response:
     """Получить среднее значение курса валютной пары за прошедшие 10 минут"""
     try:
-        # проверяем, есть ли такая пара в БД
-        if not pair in app.db_layer.db_pairs.collection_names():
+        now = datetime.datetime.utcnow()
+        if (not app.db_layer.all_pairs_cached_time) or (app.db_layer.all_pairs_cached_time + datetime.timedelta(
+                minutes=10) < now):
+            # обновить список пар в кеше
+            app.db_layer.all_pairs_cached = [pair for pair in app.db_layer.db_pairs.collection_names()]
+            app.db_layer.all_pairs_cached_time = datetime.datetime.now()
+
+        # Проверяем что такая пара есть
+        if pair not in app.db_layer.all_pairs_cached:
             return Response({
                 'message': f'Нет данных для такой пары: {pair}'
             }, status=400)
-        now = datetime.datetime.utcnow()
-        time_condition = {'$gte': now + datetime.timedelta(minutes=-10), '$lt': now}
-        average = app.db_layer.db_pairs[pair].aggregate([
-            {"$match": {"time": time_condition}},
-            {"$group": {"_id": None, "average_price": {"$avg": "$value"}}}
-        ])
-        average_num = list(average)[0]['average_price']
-        return Response({
-            'pair': pair,
-            'average': str(average_num),
-        }, status=200)
+
+        # сначала ищем в кеше и проверяем кто кеш свежий
+        if pair not in app.db_layer.cache:
+            cache_pair(now, pair)
+
+        # возвращем из кеша всегда
+        cached_time, cached_price = app.db_layer.cache[pair]
+        if cached_time + datetime.timedelta(minutes=1) < now:
+            # кеш устарел - кешируем заново
+            cache_pair(now, pair)
+        else:
+            # возвращаем из кеша
+            return Response({
+                'average': str(cached_price),
+            }, status=200)
     except Exception as ex:
         return Response({
             'message': f'Ошибка при получении курса для пары {pair}: {ex}'
         }, status=500)
+
+
+def cache_pair(now, pair):
+    # получаем цену, кешим
+    time_condition = {'$gte': now - datetime.timedelta(minutes=10), '$lt': now}
+    average = app.db_layer.db_pairs[pair].aggregate([
+        {"$match": {"time": time_condition}},
+        {"$group": {"_id": None, "average_price": {"$avg": "$value"}}}
+    ])
+    average_price = list(average)[0]['average_price']
+    app.db_layer.cache[pair] = (now, average_price)
 
 
 routes = [
